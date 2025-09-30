14496d918c71c7386bcba2572fd309356d1be4bf
Sankalp Gupta <31817289+sankalpsg@users.noreply.github.com>
2025-09-30 18:52:38 +0100

    Create config.py

---
+++ src/config.py
@@ -0,0 +1,18 @@
+# config.py
+
+# Your league ID
+LEAGUE_ID = 665400  # <-- replace with your league
+
+# How you map “months” (or custom periods) to official gameweeks
+MONTH_TO_GWS = {
+    1: [1, 2, 3, 4],
+    2: [5, 6, 7, 8],
+    3: [9, 10, 11, 12],
+    4: [13, 14, 15, 16],
+    5: [17, 18, 19]
+    # etc…
+}
+
+# (Optional) If you have credentials (email/password) for protected endpoints
+USERNAME = guptasa@tcd.ie
+PASSWORD = Sankashti12*
